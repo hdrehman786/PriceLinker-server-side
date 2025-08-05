59c1dc4207757246da5cabf4144ef22221f7235a
HD Rehman <161239883+hdrehman786@users.noreply.github.com>
2025-08-05 16:52:17 +0500

    deleted all extra commits

--- Server/app.js
+++ Server/app.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(cors({
-    origin : "https://phonelinkerclient.vercel.app",
+    origin : "http://localhost:5173",
     credentials : true
 }))
 
